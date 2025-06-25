e35b0f3ea8fceffe033bd6c51d7d97275315b135
Teodors Lisovenko <teodors.lisovenko@unive.it>
2025-06-25 16:41:42 +0200

    Implementation of #3 task
    Added the possibility to run the CLI with just one command.
    
    That is with just:
    ```bash
    main.py analyse --benchdir […] --lisadir […]  --outdir  […]
    
    or even shorter…
    
    main.py analyse —b […] —l […]  —o  […]
    ```

--- cli/commands/harvest.py
+++ cli/commands/harvest.py
@@ -2,7 +2,7 @@
 import os
 import json
 from pathlib import Path
-from typing import List
+from typing import List, Optional
 
 # Load vendored packages
 from vendor.package_loader import load_packages
@@ -23,6 +23,7 @@ from cli.models.task_definition.task_definition import TaskDefinition
 cli = typer.Typer()
 config = Config.get()
 
+
 @cli.command()
 def harvest():
     """
@@ -34,11 +35,25 @@ def harvest():
         raise typer.Exit()
 
     rich.print("[yellow]Harvesting task definitions from SV-COMP benchmark directory...[/yellow]")
-    __construct_task_definition(__harvest_task_definitions())
+
+    definitions = fetch_tasks()
+    __save_tasks(definitions)
+
+def fetch_tasks(benchmark_dir_path_from_cli: Optional[Path] = None) -> list[TaskDefinition]:
+    """
+        Main function to harvest task definitions. Left as public for other commands to use.
+    """
+    raw_task_files = __harvest_tasks(benchmark_dir_path_from_cli)
+    definitions = __construct_task_definition(raw_task_files)
+
+    return definitions
 
 
-def __harvest_task_definitions() -> list[str]:
+def __harvest_tasks(benchmark_dir_path_from_cli: Optional[Path] = None) -> list[str]:
     paths_to_definition_files: list[str] = []
+
+    if benchmark_dir_path_from_cli:
+        config.path_to_sv_comp_benchmark_dir = benchmark_dir_path_from_cli
 
     for root, dirs, files in os.walk(config.path_to_sv_comp_benchmark_dir / "java"):
         for file in files:
@@ -47,14 +62,15 @@ def __harvest_task_definitions() -> list[str]:
 
     return paths_to_definition_files
 
-def __construct_task_definition(paths_to_definition_files: list[str]) -> None:
+
+def __construct_task_definition(paths_to_definition_files: list[str]) -> list[TaskDefinition]:
     definitions: List[TaskDefinition] = []
 
     for path_str in paths_to_definition_files:
         path = Path(path_str)
 
         try:
-            with path.open("r") as stream:
+            with path.open() as stream:
                 task_data = yaml.safe_load(stream)
         except yaml.YAMLError as e:
             rich.print(f"[bold red]Error parsing YAML file:[/bold red] {path}\n{e}")
@@ -62,6 +78,7 @@ def __construct_task_definition(paths_to_definition_files: list[str]) -> None:
         except FileNotFoundError:
             rich.print(f"[bold red]File not found:[/bold red] {path}")
             continue
+
         input_files = ""
         for file in task_data["input_files"]:
             input_file = (
@@ -86,6 +103,11 @@ def __construct_task_definition(paths_to_definition_files: list[str]) -> None:
             input_file=input_files,
             properties=properties
         ))
+
+    return definitions
+
+
+def __save_tasks(definitions: list[TaskDefinition]) -> None:
 
     tasks_file: Path = config.path_to_output_dir / "tasks.json"
     tasks_file.write_text(json.dumps(definitions, indent=4, default=json_serializer))
