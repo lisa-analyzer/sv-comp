d629ae1fab8330326afe4744a14992f54828254d
Luca Negrini <luca.negrinii@gmail.com>
2025-10-29 10:09:01 +0100

    parallel execution  in analyse

--- cli/commands/analyse.py
+++ cli/commands/analyse.py
@@ -6,6 +6,8 @@ import time
 import shutil
 import os
 import signal
+from concurrent.futures import ThreadPoolExecutor
+from threading import Lock
 
 # Load vendored packages
 from vendor.package_loader import load_packages
@@ -26,7 +28,16 @@ from cli.models.task_definition.task_definition import TaskDefinition
 cli = typer.Typer()
 config = Config.get()
 
-DEFAULT_TIMEOUT = 5 * 60 # 5 minutes
+class WorkerTask:
+    def __init__(self, task: TaskDefinition, start_time: float, timeout: int, max_memory: int, timed_out: list[str], total_tasks: int, task_idx: int, lock: Lock):
+        self.task = task
+        self.start_time = start_time
+        self.timeout = timeout
+        self.max_memory = max_memory
+        self.timed_out = timed_out
+        self.total_tasks = total_tasks
+        self.task_idx = task_idx
+        self.lock = lock
 
 @cli.command()
 def analyse(
@@ -46,7 +57,15 @@ def analyse(
         timeout: Annotated[Optional[int], typer.Option(
             "--timeout", "-t",
             help="Timeout for each analysis in seconds"
-        )] = DEFAULT_TIMEOUT
+        )] = 300, # 5 minutes
+        max_memory: Annotated[Optional[int], typer.Option(
+            "--max-memory", "-m",
+            help="Maximum memory for each analysis in GB"
+        )] = 10, # 10 GB
+        parallelism: Annotated[Optional[int], typer.Option(
+            "--parallelism", "-p",
+            help="Number of parallel analyses to run"
+        )] = 1,
 ):
     """
         Sends collected tasks to the LiSA instance for analysis
@@ -71,50 +90,18 @@ def analyse(
     workdir = f"{str(config.path_to_output_dir)}/results"
     if os.path.exists(workdir):
         shutil.rmtree(workdir, ignore_errors=True)
-
-    __perform_analysis(tasks, timeout)
-
-
-def __perform_analysis(tasks: list[TaskDefinition], timeout: int):
-    total = len(tasks)
-    count = 1
+    
     start_time = time.time()
     timed_out = []
+    total_tasks = len(tasks)
+    lock = Lock()
     
-    for task in tasks:
-        command = (f"java"
-                   f" -Xmx10G"
-                   f" -cp {config.path_to_lisa_instance}"
-                   f" it.unive.jlisa.Main"
-                   f" -s {task.input_file}"
-                   f" -o {str(config.path_to_output_dir)}/results/{str(task.file_name)}"
-                   f" -n ConstantPropagation"
-                   f" -m Statistics "
-                   f" -c Assert"
-                   f" --no-html"
-                   )
-
-        rich.print(f"Running command {count}/{total}: [bold blue]{command}[/bold blue]")
-        proc = subprocess.Popen(command, shell=True, preexec_fn=os.setsid)
-        try:
-            proc.wait(timeout=timeout)
-            if proc.returncode != 0:
-                raise subprocess.CalledProcessError(proc.returncode, command)
-            rich.print("[green]Command executed.[/green]")
-        except subprocess.TimeoutExpired:
-            rich.print("[yellow]Command timed out, waiting for termination...[/yellow]")
-            os.killpg(os.getpgid(proc.pid), signal.SIGKILL)
-            proc.wait()
-            timed_out.append(str(task.file_name))
-            rich.print("[yellow]Terminated.[/yellow]")
-        except Exception as e:
-            print(f"An unexpected error occurred: {e}", file=sys.stderr)
-            rich.print("[red]Command failed.[/red]")
-            
-        elapsed = time.time() - start_time
-        elapsed_hms = time.strftime('%H:%M:%S', time.gmtime(elapsed))
-        rich.print(f"[yellow]Elapsed time since beginning: {elapsed_hms}[/yellow]")
-        count += 1
+    with ThreadPoolExecutor(max_workers=parallelism) as executor:
+        i = 1
+        for task in tasks:
+            tsk = WorkerTask(task, start_time, timeout, max_memory, timed_out, total_tasks, i, lock)
+            i += 1
+            executor.submit(__perform_analysis, tsk)
     
     if timed_out:
         rich.print("[red]The following tasks timed out:[/red]")
@@ -122,3 +109,42 @@ def __perform_analysis(tasks: list[TaskDefinition], timeout: int):
             for t in timed_out:
                 rich.print(f"[red]- {t}[/red]")
                 f.write(f"{t}\n")
+
+def __perform_analysis(task: WorkerTask):
+    command = (f"java"
+                f" -Xmx{task.max_memory}G"
+                f" -cp {config.path_to_lisa_instance}"
+                f" it.unive.jlisa.Main"
+                f" -s {task.task.input_file}"
+                f" -o {str(config.path_to_output_dir)}/results/{str(task.task.file_name)}"
+                f" -n ConstantPropagation"
+                f" -m Statistics "
+                f" -c Assert"
+                f" --no-html"
+                f" --l ERROR"
+            )
+
+    rich.print(f"Running command {task.task_idx}/{task.total_tasks}: [bold blue]{command}[/bold blue]")
+    proc = subprocess.Popen(command, shell=True, preexec_fn=os.setsid)
+    try:
+        proc.wait(timeout=task.timeout)
+        if proc.returncode != 0:
+            raise subprocess.CalledProcessError(proc.returncode, command)
+        elapsed = time.time() - task.start_time
+        elapsed_hms = time.strftime('%H:%M:%S', time.gmtime(elapsed))
+        rich.print(f"[green]Command {task.task_idx} successful. Elapsed time: {elapsed_hms}[/green]")
+    except subprocess.TimeoutExpired:
+        rich.print(f"[yellow]Command {task.task_idx} timed out, waiting for termination...[/yellow]")
+        os.killpg(os.getpgid(proc.pid), signal.SIGKILL)
+        proc.wait()
+        with task.lock:
+            task.timed_out.append(str(task.task.file_name))
+        elapsed = time.time() - task.start_time
+        elapsed_hms = time.strftime('%H:%M:%S', time.gmtime(elapsed))
+        rich.print(f"[yellow]Command {task.task_idx} terminated. Elapsed time: {elapsed_hms}[/yellow]")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}", file=sys.stderr)
+        elapsed = time.time() - task.start_time
+        elapsed_hms = time.strftime('%H:%M:%S', time.gmtime(elapsed))
+        rich.print(f"[red]Command {task.task_idx} failed. Elapsed time: {elapsed_hms}[/red]")
+
